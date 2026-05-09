748c6b38a9f9f8d350105a53b021d8d7b510dd88
creoninte-star <creoninte@gmail.com>
2026-05-09 13:19:52 +0530

    Update secondary contact phone number

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -241,7 +241,7 @@ export default function Contact() {
                   <p className={styles.infoText}>
                     <a href="tel:+919383448172">+91 9383448172</a>
                     <br />
-                    <a href="tel:+919048692086">+91 9048692086</a>
+                    <a href="tel:+917736036076">+91 7736036076</a>
                   </p>
                 </div>
               </div>
